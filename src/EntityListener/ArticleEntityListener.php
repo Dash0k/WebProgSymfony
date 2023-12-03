18aa8858b2adfc122bb32ca2e3ac4c2b33595393
Dasha <distomina87@mail.ru>
2023-12-03 20:11:04 +0700

    Add frontend and translation

--- src/EntityListener/ArticleEntityListener.php
+++ src/EntityListener/ArticleEntityListener.php
@@ -1,25 +1,26 @@
 <?php
-
-declare(strict_types=1);
-
-namespace App\EntityListener;
-
+declare(strict_types= 1);
+namespace App\EntityListeners;
+ 
+// use Doctrine\Migrations\Events; 
+// use Symfony\Component\EventDispatcher\Attribute\AsEventListener; 
+ 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Events;
 use Symfony\Bundle\SecurityBundle\Security;
-
-#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: Article::class)]
+ 
+#[AsEntityListener(event:Events::prePersist, method:'prePersist', entity: Article::class)]
 class ArticleEntityListener
 {
     public function __construct(private readonly Security $security)
     {
+ 
     }
-
     public function prePersist(PrePersistEventArgs $event): void
     {
-        /** @var Article $entity */
+        /**@var Article $entity */
         $entity = $event->getObject();
         $entity->setCreatedAt(new \DateTimeImmutable());
         $entity->setAuthor($this->security->getUser());
